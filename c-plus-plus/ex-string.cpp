817cf8e734fd2af43dc5f3fccc8b5b9e20aa15a3
Tom Bartolucci <tbartolucci@billtrust.com>
2017-06-14 09:00:30 -0400

    Finished String example

---
+++ c-plus-plus/ex-string.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string word1;
+    string word2;	
+
+    cout << "Please enter the first word: " << endl;
+    //cin >> word1;
+    getline(cin, word1);
+    cout << "Please enter the second word: " << endl;
+    //cin >> word2;
+    getline(cin, word2);
+    
+    if( word1.length() > word2.length() ){
+        cout << "The first word is longer than the second" << endl;
+    }else if ( word2.length() > word1.length() ){
+        cout << "The second word is longer than the first" << endl;
+    }else {
+        cout << "The words are equal in length" << endl;
+    }
+
+	return 0;
+}
