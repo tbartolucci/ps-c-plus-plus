3b5f5d05fa606ea93389a903e779c535fd8b436f
Tom Bartolucci <tbartolucci@billtrust.com>
2017-06-15 07:34:01 -0400

    Finished Vector Demo

--- collections/main.cpp
+++ collections/main.cpp
@@ -12,8 +12,22 @@ int main()
         vi.push_back(i);
     }
 
+    vi[5] = 3;
+    vi[6] = -1;
+    vi[1] = 99;
+
     for( auto item:vi ){
         cout << item << " ";
+    }
+    cout << endl;
+
+    for ( unsigned int i=0; i < vi.size(); i++ ){
+        cout << vi[i] << " " ;
+    }
+    cout << endl;
+
+    for( auto i = begin(vi); i != end(vi); i++ ){
+        cout << *i << " ";
     }
     cout << endl;
 
@@ -25,9 +39,18 @@ int main()
         vs.push_back(s);
     }
 
+    sort(begin(vs), end(vs));
     for( auto item:vs){
         cout << item << " ";
     }
     cout << endl;
+
+    int threes = count(begin(vi), end(vi), 3);
+    cout << "vector of ints has " << threes << " elements with value of 3" << endl;
+
+    //string is a collection
+    int tees = count(begin(vs[0]), end(vs[0]),'t');
+    cout << "first word has " << tees << " letter T's" << endl;
+
     return 0;
 }
